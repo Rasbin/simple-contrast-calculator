3e0770f74de6bbcaa8f4a56e22f48efa13a876cd
Rijal Rasbin <rarijal@IT-L-M00202.local>
2019-12-09 12:37:30 +0200

    Update color value from input field of color

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -8,6 +8,22 @@ class ColorBox extends React.Component {
     hexColor3: '#9B9B9B',
   }
 
+  handleColorChange = (e) => {
+    e.preventDefault();
+    this.props.colorNumber === 1
+    ? this.setState({
+      hexColor1: e.target.value
+    })
+    : (this.props.colorNumber === 2
+    ? this.setState({
+      hexColor2: e.target.value
+    })
+    : this.setState({
+      hexColor3: e.target.value
+    })
+    );
+  }
+
   render() {
     let displayColor =  this.props.colorNumber === 1
     ? this.state.hexColor1
@@ -15,7 +31,7 @@ class ColorBox extends React.Component {
     ? this.state.hexColor2
     : this.state.hexColor3
     );
-    
+
     return (
       <div className="colorBox">
         <h2>Color {this.props.colorNumber}</h2>
@@ -23,7 +39,7 @@ class ColorBox extends React.Component {
           className="displayColor"
           style={{backgroundColor: displayColor}}>
         </div>
-        <p>Hue</p>
+        <div className="picker"></div>
         <p>Saturation</p>
         <p>Brightness</p>
         <p>Hex</p>
@@ -37,6 +53,7 @@ class ColorBox extends React.Component {
             : this.state.hexColor3
             )
           }
+          onChange={this.handleColorChange}
         />
       </div>
     );

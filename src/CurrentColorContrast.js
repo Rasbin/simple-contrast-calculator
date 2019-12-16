9b593df0c2384101d87d76f47f7c49952c2ce6ee
Rijal Rasbin <rarijal@IT-L-M00202.local>
2019-12-16 16:13:47 +0200

    Add contrast ratio check symbol

--- src/CurrentColorContrast.js
+++ src/CurrentColorContrast.js
@@ -79,21 +79,47 @@ class CurrentColorContrast extends React.Component {
     
     return (
       <div className="currentContrastBox">
-        <h2>Current color contrast</h2>
-        <p>Color 1,  Color 2  :  
-          <span>
+        <h4>Current color contrast<span style={{marginLeft: 15}}>3</span><span style={{marginLeft: 15}}>4.5</span></h4>
+        <p>Color 1,  Color 2  
+          <span style={{marginLeft: 15}}>
             { ((color1len === 4 || color1len === 7) && (color2len === 4 || color2len === 7)) ? calcContrast(this.props.hexColor1, this.props.hexColor2) : ''}
           </span>
+
+          {((color1len === 4 || color1len === 7) && (color2len === 4 || color2len === 7))
+          ? calcContrast(this.props.hexColor1, this.props.hexColor2) >= 3
+          ? <span style={{marginLeft: 15}}>&#10003;</span> : <span style={{marginLeft: 15}}>&#10007;</span> : ''}
+
+          {((color1len === 4 || color1len === 7) && (color2len === 4 || color2len === 7))
+          ? calcContrast(this.props.hexColor1, this.props.hexColor2) >= 4.5
+          ? <span style={{marginLeft: 15}}>&#10003;</span> : <span style={{marginLeft: 15}}>&#10007;</span> : ''}
+          
+
         </p>
-        <p>Color 1,  Color 3  :  
-          <span>
+        <p>Color 1,  Color 3
+          <span style={{marginLeft: 15}}>
             { ((color1len === 4 || color1len === 7) && (color3len === 4 || color3len === 7)) ? calcContrast(this.props.hexColor1, this.props.hexColor3) : ''}
           </span>
+          {((color1len === 4 || color1len === 7) && (color2len === 4 || color2len === 7))
+          ? calcContrast(this.props.hexColor1, this.props.hexColor3) >= 3
+          ?<span style={{marginLeft: 15}}>&#10003;</span> : <span style={{marginLeft: 15}}>&#10007;</span> : ''}
+
+          {((color1len === 4 || color1len === 7) && (color2len === 4 || color2len === 7))
+          ? calcContrast(this.props.hexColor1, this.props.hexColor3) >= 4.5
+          ? <span style={{marginLeft: 15}}>&#10003;</span> : <span style={{marginLeft: 15}}>&#10007;</span> : ''}
+
         </p>
-        <p>Color 2,  Color 3  :  
-          <span>
+        <p>Color 2,  Color 3
+          <span style={{marginLeft: 15}}>
             { ((color2len === 4 || color2len === 7) && (color3len === 4 || color3len === 7)) ? calcContrast(this.props.hexColor2, this.props.hexColor3) : ''}
           </span>
+          {((color1len === 4 || color1len === 7) && (color2len === 4 || color2len === 7))
+          ? calcContrast(this.props.hexColor2, this.props.hexColor3) >= 3
+          ?<span style={{marginLeft: 15}}>&#10003;</span> : <span style={{marginLeft: 15}}>&#10007;</span> : ''}
+
+          {((color1len === 4 || color1len === 7) && (color2len === 4 || color2len === 7))
+          ? calcContrast(this.props.hexColor2, this.props.hexColor3) >= 4.5
+          ? <span style={{marginLeft: 15}}>&#10003;</span> : <span style={{marginLeft: 15}}>&#10007;</span> : ''}
+
         </p>
       </div>
     );

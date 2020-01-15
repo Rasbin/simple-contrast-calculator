5660d9d0bff6313142ca5dd19c592f0d377021af
Rijal Rasbin <rarijal@IT-L-M00202.local>
2020-01-15 12:05:55 +0200

    Minor changes

--- src/components/ColorResults.js
+++ src/components/ColorResults.js
@@ -2,67 +2,70 @@ import React from 'react'
 
 const ColorResults=(props)=>{
   const {resultColor, selectedColor="", color1, color2, color3, selectedNumber="1213"} = props; // 12, selectedColor=1
-  console.log('resultColor ',resultColor, selectedColor) 
+  console.log('resultColor ', resultColor, 'Selected Color ', selectedColor)
   const result= selectedColor && selectedNumber.includes(selectedColor) ? selectedColor: "***"
 
-  return(
-    <div className="mainColorResultContainer">
-        <div className="13changeColor1">
-        {
-          selectedColor && selectedNumber.includes(selectedColor)
-            ? <p>
-                If you change Color {result} to : {resultColor}, your CR is
-                good enough.
-              </p>
-          : null
-        }
-          <br />
+  const resultDiv = <div className="mainColorResultContainer">
+  {
+    selectedColor && selectedNumber.includes(selectedColor)
+      ? 
+  <div className="13changeColor1">
+      <p>
+        If you change Color {result} to : {resultColor}, your CR is
+        good enough.
+      </p>
+  
+    <br />
 
-          {
-            arr.map((each, i)=>{
-              return(
-                <div key={i}>
-                  <span>{i}</span>
-                  <span>{each.name}</span>
-                  <span>{each.name}</span>
-                </div>
-              )
-            })
-          }
+    {/* {
+      arr.map((each, i)=>{
+        return(
+          <div key={i}>
+            <span>{i}</span>
+            <span>{each.name}</span>
+            <span>{each.name}</span>
+          </div>
+        )
+      })
+    } */}
 
-          <div
-            style={{
-              display: "inline-block",
-              backgroundColor: selectedColor=='1' && selectedNumber.includes('1')  ? resultColor:color1,
-              height: "50px",
-              width: "250px",
-              border: "2px solid grey",
-              borderRadius: 5
-            }}
-          ></div>
-          <div
-            style={{
-              display: "inline-block",
-              backgroundColor: selectedColor=='2'&& selectedNumber.includes('2')  ? resultColor:color2,
-              height: "50px",
-              width: "250px",
-              border: "2px solid grey",
-              borderRadius: 5
-            }}
-          ></div>
-          <div
-            style={{
-              display: "inline-block",
-              backgroundColor: selectedColor=='3'&& selectedNumber.includes('3')  ? resultColor:color3,
-              height: "50px",
-              width: "250px",
-              border: "2px solid grey",
-              borderRadius: 5
-            }}
-          ></div>
-        </div>
-      </div>
-    
+    <div
+      style={{
+        display: "inline-block",
+        backgroundColor: selectedColor=='1' && selectedNumber.includes('1')  ? resultColor:color1,
+        height: "50px",
+        width: "250px",
+        border: "2px solid grey",
+        borderRadius: 5
+      }}
+    ></div>
+    <div
+      style={{
+        display: "inline-block",
+        backgroundColor: selectedColor=='2'&& selectedNumber.includes('2')  ? resultColor:color2,
+        height: "50px",
+        width: "250px",
+        border: "2px solid grey",
+        borderRadius: 5
+      }}
+    ></div>
+    <div
+      style={{
+        display: "inline-block",
+        backgroundColor: selectedColor=='3'&& selectedNumber.includes('3')  ? resultColor:color3,
+        height: "50px",
+        width: "250px",
+        border: "2px solid grey",
+        borderRadius: 5
+      }}
+    ></div>
+  </div>
+  : null
+  }
+</div>
+
+  return (
+    resultDiv
   )
 }
 
